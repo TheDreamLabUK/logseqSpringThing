68f66980793ee82f853a53b550a4efb4b25d9df9
flossverse <github@xrsystems.uk>
2024-12-14 18:07:54 +0000

    written out a plan

--- featureclient/utils/three.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-// Export everything from three
-export * from 'three';
-// Export specific imports
-export { OrbitControls };
-// Export THREE as default
-export default THREE;
