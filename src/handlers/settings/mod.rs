0ded891ef2ab66b6d25c55752c4bec0978e81cef
flossverse <github@xrsystems.uk>
2024-12-24 19:13:04 +0000

    different error at least

--- src/handlers/settings/mod.rs
+++ src/handlers/settings/mod.rs
@@ -10,6 +10,6 @@ use actix_web::web;
 
 // Register all settings handlers
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(visualization::config)
-       .service(websocket::config);
+    visualization::config(cfg);
+    websocket::config(cfg);
 }
